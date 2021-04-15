26fbf905ed0cdcf8796b239ed556a2e1005fa8a0
Harim Lee <devharimm@gmail.com>
2021-04-16 03:57:27 +0900

    Add TBL_STUDENT-related views

--- Codes/PRC_STUDENT_이하림.sql
+++ Codes/PRC_STUDENT_이하림.sql
@@ -1,4 +1,4 @@
---○ 테스트(dummy data)
+--○ 프로시저 테스트(dummy data)
 ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
 
 SELECT *
@@ -32,6 +32,7 @@ EXEC PRC_STUDENT_DELETE('S10005');
 
 
 --○ 관리자 계정에서 등록된 학생 출력(ADMIN_STUDENT_VIEW)
+-- 출력 정보 : 학생 이름, 과정명, 수강과목, 수강과목 총점
 CREATE OR REPLACE VIEW ADMIN_STUDENT_VIEW
 AS
 SELECT S.S_NAME "학생이름"
@@ -39,13 +40,197 @@ SELECT S.S_NAME "학생이름"
      , SJ.SJ_NAME "수강과목"
      , (SC.A_SCORE + SC.W_SCORE + SC.P_SCORE) "수강과목총점"
 FROM TBL_STUDENT S, TBL_SUGANG SG, TBL_COURSE C, TBL_OP_COURSE OPC, TBL_SUBJECT SJ, TBL_OP_SUBJECT OPS, TBL_SCORE SC
-WHERE S.S_CODE = SG.S_CODE
-  AND SG.OPC_CODE = OPC.OPC_CODE
-  AND OPC.OPC_CODE = OPS.OPC_CODE
-  AND OPC.C_CODE = C.C_CODE
-  AND OPS.SJ_CODE = SJ.SJ_CODE
-  AND SG.SG_CODE = SC.SG_CODE
-  AND SC.OPS_CODE = OPS.OPS_CODE;
+WHERE S.S_CODE = SG.S_CODE(+)
+  AND SG.OPC_CODE = OPC.OPC_CODE(+)
+  AND OPC.OPC_CODE = OPS.OPC_CODE(+)
+  AND OPS.OPS_CODE = SC.OPS_CODE(+)
+  AND OPC.C_CODE = C.C_CODE(+)
+  AND OPS.SJ_CODE = SJ.SJ_CODE(+);
 --==>> View ADMIN_STUDENT_VIEW이(가) 생성되었습니다.
+
+SELECT *
+FROM ADMIN_STUDENT_VIEW;
+--==>>
+/*
+학생이름	과정명	            수강과목	수강과목총점
+박민지	자바 개발자 양성과정	    자바	    240
+심혜진			
+이하림			
+이희주			
+전혜림				
+*/
+
+  
+--○ 학생 계정에서 과목 출력(STUDENT_SUBJECT_VIEW)
+-- 수강이 끝난 과목만 출력 가능
+CREATE OR REPLACE VIEW STUDENT_SUBJECT_VIEW
+AS
+SELECT S.S_NAME "학생명", SJ.SJ_NAME "과목명"
+FROM TBL_STUDENT S, TBL_SUGANG SG, TBL_OP_COURSE OPC, TBL_SUBJECT SJ, TBL_OP_SUBJECT OPS
+WHERE S.S_CODE = SG.S_CODE(+)
+  AND SG.OPC_CODE = OPC.OPC_CODE(+)
+  AND OPS.OPC_CODE(+) = OPC.OPC_CODE
+  AND OPS.SJ_CODE = SJ.SJ_CODE(+);
+--==>> View STUDENT_SUBJECT_VIEW이(가) 생성되었습니다.
+
+SELECT *
+FROM STUDENT_SUBJECT_VIEW
+WHERE 학생명 = '박민지';
+--==>> 박민지	자바
+
+SELECT *
+FROM STUDENT_SUBJECT_VIEW
+WHERE 과목명 = '자바';
+--==>> 박민지	자바
+
+
+--○ 학생 계정에서 성적 출력(STUDENT_SCORE_VIEW)
+-- 수강이 끝난 과목 성적 확인 가능 
+-- 특정 과목 선택 시, 그 과목의 점수만 보여줄 수 있게.
+-- 출력 정보 : 학생이름, 과정명, 과목명, 교육기간(시작 연월일, 끝연월일), 교재명, 출결, 실기, 필기, 총점, 등수 
+CREATE OR REPLACE VIEW STUDENT_SCORE_VIEW
+AS
+SELECT S.S_NAME "학생이름"
+     , C.C_NAME "과정명"
+     , SJ.SJ_NAME "과목명"
+     , OPS.START_DATE "교육시작일"
+     , OPS.END_DATE "교육마지막일"
+FROM TBL_STUDENT S, TBL_SUGANG SG, TBL_COURSE C, TBL_OP_COURSE OPC, TBL_SUBJECT SJ, TBL_OP_SUBJECT OPS, TBL_SCORE SC
+WHERE S.S_CODE = SG.S_CODE(+)
+  AND SG.OPC_CODE = OPC.OPC_CODE(+)
+  AND OPC.OPC_CODE = OPS.OPC_CODE(+)
+  AND OPC.C_CODE = C.C_CODE(+)
+  AND OPS.SJ_CODE = SJ.SJ_CODE(+)
+  AND SC.SG_CODE(+) = SG.SG_CODE;
+--==>> View STUDENT_SCORE_VIEW이(가) 생성되었습니다.
+
+SELECT *
+FROM STUDENT_SCORE_VIEW;
+--==>>
+/*
+학생이름	과정명	            과목명	교육시작일	    교육마지막일
+박민지	자바 개발자 양성과정	    자바	    2021-02-01	2021-03-30
+심혜진				
+이하림				
+이희주				
+전혜림							
+*/
+
+SELECT *
+FROM TBL_STUDENT;
+
+
+--○ 뷰 테스트(dummy data)
+-- 필요한 데이터 : (학생), 교수, 책, 과정, 과목, 강의실, 과정개설, 중도탈락, 과목개설, 성적, 수강
+
+-- 교수
+INSERT INTO TBL_PROFESSOR(P_CODE, P_NAME, P_SSN, P_ID, P_PW)
+VALUES('P10001', '홍길동', '751211-1123456', '01012341234', '1123456');
+--==>> 1 행 이(가) 삽입되었습니다.
+
+SELECT *
+FROM TBL_PROFESSOR;
+--==>> P10001	홍길동	751211-1123456	01012341234	1123456	2021-04-16
+
+
+-- 책
+INSERT INTO TBL_BOOK(B_CODE, B_NAME) VALUES('B101' , 'JAVA의정석');
+INSERT INTO TBL_BOOK(B_CODE, B_NAME) VALUES('B102' , '웹표준의정석');
+--==>> 1 행 이(가) 삽입되었습니다. * 2
+
+SELECT *
+FROM TBL_BOOK;
+--==>>
+/*
+B101	JAVA의정석
+B102	웹표준의정석
+*/
+
+
+-- 과정
+INSERT INTO TBL_COURSE(C_CODE, C_NAME)
+VALUES ('C101', '자바 개발자 양성과정');
+INSERT INTO TBL_COURSE(C_CODE, C_NAME)
+VALUES ('C102', '빅데이터 개발자 양성');
+--==>> 1 행 이(가) 삽입되었습니다. * 2
+
+SELECT *
+FROM TBL_COURSE;
+--==>> 
+/*
+C102	빅데이터 개발자 양성
+C101	자바 개발자 양성과정
+*/
+
+
+-- 과목
+INSERT INTO TBL_SUBJECT(SJ_CODE, SJ_NAME)
+VALUES ('J101', '자바');
+INSERT INTO TBL_SUBJECT(SJ_CODE, SJ_NAME)
+VALUES ('J102', '오라클');
+INSERT INTO TBL_SUBJECT(SJ_CODE, SJ_NAME)
+VALUES ('J103', 'HTML');
+--==>> 1 행 이(가) 삽입되었습니다. * 3
+
+SELECT *
+FROM TBL_SUBJECT;
+--==>>
+/*
+J103	HTML
+J102	오라클
+J101	자바
+*/
+
+
+-- 강의실
+INSERT INTO TBL_ROOM(R_CODE, R_NAME, EXPALIN)
+VALUES ('R101', 'A강의장', '30명 수용가능'); 
+INSERT INTO TBL_ROOM(R_CODE, R_NAME, EXPALIN)
+VALUES ('R102', 'B강의장', '20명 수용가능'); 
+--==>> 1 행 이(가) 삽입되었습니다. * 2
+
+SELECT *
+FROM TBL_ROOM;
+--==>> 
+/*
+R101	A강의장	30명 수용가능
+R102	B강의장	20명 수용가능
+*/
+
+
+-- 과정개설
+INSERT INTO TBL_OP_COURSE(OPC_CODE, C_CODE, R_CODE, START_DATE, END_DATE)
+VALUES(1001, 'C101', 'R101', TO_DATE('2021-02-01'), TO_DATE('2021-07-02'));
+--==>> 1 행 이(가) 삽입되었습니다.
+
+
+-- 배점
+INSERT INTO TBL_ALLOT(AL_CODE, A_ALLOT, W_ALLOT, P_ALLOT)
+VALUES(1001, 20, 50, 30);
+--==>> 1 행 이(가) 삽입되었습니다.
+
+
+-- 수강
+INSERT INTO TBL_SUGANG(SG_CODE, S_CODE, OPC_CODE, SG_DATE)
+VALUES(1001, 'S10001', 1001, SYSDATE);
+--==>> 1 행 이(가) 삽입되었습니다.
+
+
+-- 중도탈락
+INSERT INTO TBL_DROP(D_CODE, SG_CODE, REASON, DR_DATE)
+VALUES(1001, 1001, '출석미달', SYSDATE);
+--==>> 1 행 이(가) 삽입되었습니다.
+
+
+-- 과목개설
+INSERT INTO TBL_OP_SUBJECT(OPS_CODE, SJ_CODE, OPC_CODE, P_CODE, B_CODE, AL_CODE, START_DATE, END_DATE)
+VALUES(1001, 'J101', 1001, 'P10001', 'B101', 1001, TO_DATE('2021-02-01'), TO_DATE('2021-03-30'));
+--==>> 1 행 이(가) 삽입되었습니다.
+
+
+-- 성적
+INSERT INTO TBL_SCORE (SC_CODE, SG_CODE, OPS_CODE, A_SCORE, W_SCORE, P_SCORE)
+VALUES(1001, 1001, 1001, 90, 80, 70);
+--==>> 1 행 이(가) 삽입되었습니다.
 
 
